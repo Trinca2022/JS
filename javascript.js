9709424b9aba0fd9ccc5377ef807de825cc9bba8
Agustin Trinca <catrincavelli@gmail.com>
2022-11-12 14:41:30 -0300

    primera

---
+++ javascript.js
@@ -0,0 +1,31 @@
+
+/*Usando _while_, crea un programa que pregunte al usuario un número. Mostrar los números que son múltiplos de 5 desde 1 
+hasta el número introducido por el usuario.*/
+
+/*while(numero + 5 <= respuesta) {
+    numero= numero + 5
+    console.log(numero)
+    
+}*/
+/*
+do{
+    numero++
+    if(numero % 5 === 0){
+        console.log(`Toma pa vo ${numero}`)
+    }
+}while(numero <= respuesta)
+
+*/
+let ch = 4
+
+function azul(i, j) {
+    let f = 5
+    console.log(ch)
+    if (i === f) {
+        return "Equal";
+    }
+    return "Not Equal";
+
+}
+
+azul(5, 3)
