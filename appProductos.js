92068b0d849183517bc3e2561a10cfb37c104218
Agustin Trinca <catrincavelli@gmail.com>
2022-12-16 00:21:02 -0300

    avanzo con el carrito

--- appProductos.js
+++ appProductos.js
@@ -30,11 +30,11 @@ let carritoDeProductos = []
 const listaProductos = document.getElementById("listaProductos");
 
 
-//const mostrarProductos = () => {
-productosExistentes.forEach(productoArray => {
-    const card = document.createElement("div");
-    card.classList.add("col-xl-3", "col-md-6", "col-xs-12");
-    card.innerHTML = `
+const mostrarProductos = () => {
+    productosExistentes.forEach(productoArray => {
+        const card = document.createElement("div");
+        card.classList.add("col-xl-3", "col-md-6", "col-xs-12");
+        card.innerHTML = `
             <div class="card" style="width: 15rem;">
             <img src="${productoArray.img}"
                 class="card-img-top" alt="...">
@@ -45,14 +45,54 @@ productosExistentes.forEach(productoArray => {
             </div>
         </div>
         `
-    listaProductos.appendChild(card);
+        listaProductos.appendChild(card);
 
-    const boton = document.getElementById(`boton${productoArray.id}`);
-    boton.addEventListener("click", () => { agregarAlCarrito(productoArray.id) })
+        const boton = document.getElementById(`boton${productoArray.id}`);
+        boton.addEventListener("click", () => { agregarAlCarrito(productoArray.id) })
 
 
 
-})
+    })
+}
 
-//mostrarProductos();
+const agregarAlCarrito = (id) => {
+    const productoListo = carritoDeProductos.find(productoArray => productoArray.id === id)
+    if (productoListo) {
+        productoListo.kilo++;
+    } else {
+        productoAgregado = productosExistentes.find(productoArray => productoArray.id === id);
+        carritoDeProductos.push(productoAgregado)
+    }
+    console.log(carritoDeProductos)
+}
 
+mostrarProductos();
+
+// Productos que aparecen en el carrito
+
+
+
+//const containerProductosDelCarrito = document.getElementById("containerProductosDelCarrito")
+
+const pruebaLoca = document.getElementById("pruebaLoca")
+
+const prueba = document.createElement("h1")
+prueba.innerHTML = "hola"
+
+pruebaLoca.appendChild(prueba)
+
+/*
+const productosDelCarrito = () => {
+    carritoDeProductos.forEach(productoArray => {
+        const articuloCarrito = document.createElement("li");
+        articuloCarrito.classList.add("list-group-item d-flex justify-content-between align-items-center");
+        articuloCarrito.innerHTML = `
+            Café ${productoArray.nombre}
+
+        `
+    })
+    containerProductosDelCarrito.appendChild(articuloCarrito);
+}
+
+productosDelCarrito();
+*/
